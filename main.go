bbb5e3779d897ee638954e04d4658cc887847579
turleft <867512099@qq.com>
2023-09-23 00:12:13 +0800

    feat: 注册服务到consul服务发现

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-
+	"github.com/go-micro/plugins/v4/registry/consul"
 	"hello/handler"
 	pb "hello/proto"
 
 	"go-micro.dev/v4"
 	"go-micro.dev/v4/logger"
-
 )
 
 var (
@@ -16,8 +15,15 @@ var (
 )
 
 func main() {
+	//集成consul
+	consulReg := consul.NewRegistry()
 	// Create service
 	srv := micro.NewService(
+		micro.Address("127.0.0.1:8080"), //指定微服务的ip:  选择注册服务器地址,也可以不配置,默认为本机,也可以选择consul集群中的client
+		micro.Name(service),
+		micro.Version(version),
+		//注册consul
+		micro.Registry(consulReg),
 	)
 	srv.Init(
 		micro.Name(service),
